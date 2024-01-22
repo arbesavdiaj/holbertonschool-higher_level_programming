e451ad895f9999ff58b33b3ee399e99d1f8634a9
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-01-22 13:29:10 +0100

    fix code

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,2 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i < 10:
-        i = '0' + str(i)
-        print("{}".format[i], end=", ")
-    else:
-    	print("{}".format[i], end=", ")
+print(", ".join(["{:02d}".format(i) for i in range(100)]))
