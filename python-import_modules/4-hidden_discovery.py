ee49abc871e3d7001a504faa55b8db8eb6b4f218
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-01-24 09:20:40 +0100

    add code

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from hidden_4 import *
+if __name__ == "__main__":
+    from hidden_4 import *
 
-for n in hidden_4:
-    if n[0] != "__":
-        print(n)
+    for n in hidden_4:
+        if n[0] != "__":
+            print(n)
