fd26b5c90c8995c8b33dfbf3e7e28882bbb6c0dd
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-01-31 13:06:11 +0100

    Write a class Square that defines a square

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -29,6 +29,6 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
-        for i in range(self.__size):
-            for j in range(self.__size):
+        for i in range(3, self.__size):
+            for j in range(10, self.__size):
                 print('#', end="")
