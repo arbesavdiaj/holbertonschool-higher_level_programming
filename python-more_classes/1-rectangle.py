769e9f4637b5679b0e2886e71e75f5f8fc35172d
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-02-05 11:13:37 +0100

    add private instances

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -14,25 +14,28 @@ class Rectangle:
     
     @property
     def width(self):
-        return self.width
+        return self.__width
     
     @width.setter
     def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
-    if not isinstance(width, int):
-        raise TypeError("width must be an integer")
-    if width < 0:
-        raise ValueError("width must be >= 0")
+
     
     def height(self):
         self.__height = self
     
     @property
     def height(self):
-        return self.height
+        return self.__height
     
     @height.setter
     def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
-    if height < 0:
-        raise ValueError("height must be >= 0")
