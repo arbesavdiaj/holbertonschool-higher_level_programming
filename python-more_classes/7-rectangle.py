ecc5ce1ffe3714fbd91f48b934a0b700694ef2aa
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-02-05 14:03:31 +0100

    add file

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -52,7 +52,7 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
-        return '\n'.join([str(self.print_symbol) * self.width for i in range(self.height)])
+        return (str(self.print_symbol) * self.__width + "\n") * self.__height
 
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
