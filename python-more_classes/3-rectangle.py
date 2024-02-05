018e856c8ba2fc9c63623c847a4456168f1b1af7
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-02-05 12:17:22 +0100

    print rectangle

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -49,4 +49,4 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return ""
         else:
-            return '\n'.join(['#' * self.width for _ in range(self.height)])
+            return '\n'.join(['#' * self.width for i in range(self.height)])
