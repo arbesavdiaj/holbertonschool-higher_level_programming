a3a072e979ea702db493402ec13811f77331956e
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-02-05 13:07:26 +0100

    add instances

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -42,19 +42,19 @@ class Rectangle:
         return self.__height * self.__width
 
     def perimeter(self):
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return 2 * (self.__height + self.__width)
 
     def __str__(self):
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return ""
         else:
             return '\n'.join(['#' * self.__width for i in range(self.__height)])
 
     def __repr__(self):
-        return f'Rectangle({self.width}, {self.height})'
+        return f'Rectangle({self.__width}, {self.__height})'
 
     def __del__(self):
         Rectangle.number_of_instances -= 1
