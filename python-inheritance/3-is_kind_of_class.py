741059fc8ac809c0e32a7290a39ebe7feeb401d1
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-02-07 22:29:45 +0100

    isinstance

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -2,7 +2,6 @@
 """Defines a class and inherited class-checking function."""
 
 
-
 def is_kind_of_class(obj, a_class):
     """Checks if an object is an instance of, or if the object
     is an instance of a class that inherited from,
