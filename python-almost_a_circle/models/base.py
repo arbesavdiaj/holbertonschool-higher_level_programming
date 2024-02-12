8b72957624731ebe1801ac9878741450ff4d36c5
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-02-12 11:50:37 +0100

    fix code

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+'''base class'''
+
 
 class Base:
+    '''base class'''
     __nb_objects = 0
 
     def __init__(self, id=None):
+        '''Constructor'''
         if id is not None:
             self.id = id
         Base.__nb_objects += 1
