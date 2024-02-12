179473aa4663a5646a0e1bc2ad41c4244d87b2a9
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-02-12 13:28:02 +0100

    add str

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -68,3 +68,6 @@ class Rectangle(Base):
         '''display of rectangle'''
         for _ in range(self.height):
             print("#" * self.width)
+
+    def __str__(self):
+        return f"[Rectangle] {self.id} {self.x}/{self.y} - {self.width}/{self.height}"
