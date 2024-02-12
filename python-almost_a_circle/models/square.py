3d2c98e1e5376caf485221757c3dd5c2148d2e8d
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-02-12 15:46:21 +0100

    fix code

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,5 +9,5 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.x}/{self.y}\
-                                            - {self.width}"
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
+                                                 self.y, self.width)
