c3537bf54529887938c54a00951428e348ce62e7
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-02-13 11:17:00 +0100

    add class method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -19,3 +19,13 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + ".json"
+        with open(filename, 'w') as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                file.write(Base.to_json_string(list_dicts))
