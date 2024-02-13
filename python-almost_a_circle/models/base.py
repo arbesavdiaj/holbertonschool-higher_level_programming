6e6cead9d7b943dfd21abbabc55ac0e24943a793
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-02-13 11:02:08 +0100

    add static method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,7 +16,6 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        if list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        else:
-            json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
