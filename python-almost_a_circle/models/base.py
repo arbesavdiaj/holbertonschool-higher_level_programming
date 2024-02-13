c75cda5a92b798fc998d43d07e89005bf1b06f48
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-02-13 12:06:12 +0100

    add class method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -44,7 +44,7 @@ class Base:
             new = cls(1)
         new.update(**dictionary)
         return new
-    
+
     @classmethod
     def load_from_file(cls):
         filename = cls.__name__ + ".json"
