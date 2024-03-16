76048ca2c140db5d555a461c6f10b5d62a8c299f
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-03-16 14:29:48 +0100

    Full Description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,14 +1,2 @@
 -- Full description
-#!/bin/bash
-
-# Check if the correct number of arguments are provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <database_name>"
-    exit 1
-fi
-
-# Database name
-DB_NAME="$1"
-
-# MySQL command to fetch table information
-mysql -e "SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE FROM information_schema.columns WHERE table_schema='$DB_NAME' AND table_name='first_table';"
+SHOW CREATE TABLE first_table;
