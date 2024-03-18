42e7b68916d2c024d528db3522d2cc83dd0075b1
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-03-18 10:57:38 +0100

    create user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- Root User
-CREATE USER 'username'@'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
